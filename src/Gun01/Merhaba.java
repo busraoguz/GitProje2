32c7cff395669ca73f5e3b5c8cf70ef10ae190c7
busraoguz <busraaoguz@outlook.com>
2022-10-11 22:59:12 -0400

    ucuncu taskimi geri aldim.

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,14 +3,10 @@ package Gun01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
-        for (int i = 0; i <5; i++) {
-            System.out.println("Merhaba "+i);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Merhaba " + i);
 
         }
-        int sayac=0;
-        do{
-            System.out.println("yeni merhaba");
-            sayac++;
-        }while(sayac<5);
+
     }
 }
