550456a33b901dd951e9428c3c3b3a19ef98a4eb
busraoguz <busraaoguz@outlook.com>
2022-10-11 21:22:18 -0400

    ikinci taskimi yaptim.

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,6 +2,10 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
+        System.out.println("Merhaba 1");
+        for (int i = 0; i <5; i++) {
+            System.out.println("Merhaba "+i);
 
+        }
     }
 }
